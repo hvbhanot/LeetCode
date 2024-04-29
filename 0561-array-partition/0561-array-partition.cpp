01196e42187bba77606d1e029ccd27ac81ba917c
Harsh Vardhan Bhanot <152215921+hvbhanot@users.noreply.github.com>
2024-04-28 21:42:35 -0500

    Time: 40 ms (52.2%), Space: 31.6 MB (73.79%) - LeetHub

--- 0561-array-partition/0561-array-partition.cpp
+++ 0561-array-partition/0561-array-partition.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int arrayPairSum(vector<int>& nums) {
-  sort(nums.begin(), nums.end()); // Sort the array
-  int sum = 0;
-  for (int i = 0; i < nums.size(); i += 2) {
-    sum += nums[i]; // Pick every other element (already paired due to sorting)
-  }
-  return sum;
-}
-
+       sort(nums.begin() , nums.end());
+       int max = 0;
+       for(int i =0; i< nums.size(); i+=2){
+         max += nums[i];
+       }
+       return max;
+    }
 };
